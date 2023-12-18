b06f81c451de45093d424e93bbb6a7c44361981f
Patrick He <96420427+hzz4343@users.noreply.github.com>
2023-12-18 12:46:15 -0600

    connect dashboard and login

--- client/src/pages/DashboardLayout.jsx
+++ client/src/pages/DashboardLayout.jsx
@@ -1,14 +1,26 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, redirect, useLoaderData, useNavigate } from 'react-router-dom';
 import { BigSidebar, SmallSidebar, Navbar } from '../components';
 import Wrapper from '../assets/wrappers/Dashboard';
 import { useContext, createContext, useState } from 'react';
 import { checkDefaultTheme } from '../App';
+import customFetch from '../utils/customFetch';
+import { toast } from 'react-toastify';
 
 const DashboardContext = createContext();
 
+export const loader = async () => {
+  try {
+    const { data } = await customFetch.get('/users/current-user');
+    return data;
+  } catch (error) {
+    return redirect('/');
+  }
+};
+
 const DashboardLayout = () => {
-  //temp
-  const user = { name: 'john' };
+  const { user } = useLoaderData();
+  const navigate = useNavigate();
+
   const [showSidebar, setShowSidebar] = useState(false);
   const [isDarkTheme, setIsDarkTheme] = useState(checkDefaultTheme());
 
@@ -24,7 +36,9 @@ const DashboardLayout = () => {
   };
 
   const logoutUser = async () => {
-    console.log('logout user');
+    navigate('/');
+    await customFetch.get('/auth/logout');
+    toast.success('Logging out...');
   };
 
   return (
@@ -45,7 +59,7 @@ const DashboardLayout = () => {
           <div>
             <Navbar />
             <div className="dashboard-page">
-              <Outlet></Outlet>
+              <Outlet context={{ user }}></Outlet>
             </div>
           </div>
         </main>

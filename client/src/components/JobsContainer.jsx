f5c38bf733ec9ed0054c0c25070dfa27ee1341fc
Patrick He <96420427+hzz4343@users.noreply.github.com>
2023-12-18 15:56:04 -0600

    Connect allJob page

---
+++ client/src/components/JobsContainer.jsx
@@ -0,0 +1,27 @@
+import Job from './Job';
+import { useAllJobsContext } from '../pages/AllJobs';
+import Wrapper from '../assets/wrappers/JobsContainer';
+
+const JobsContainer = () => {
+  const { data } = useAllJobsContext();
+  const { jobs } = data;
+
+  if (jobs.length === 0) {
+    return (
+      <Wrapper>
+        <h2>No jobs</h2>
+      </Wrapper>
+    );
+  }
+  return (
+    <Wrapper>
+      <div className="jobs">
+        {jobs.map((job) => {
+          return <Job key={job._id} {...job}></Job>;
+        })}
+      </div>
+    </Wrapper>
+  );
+};
+
+export default JobsContainer;

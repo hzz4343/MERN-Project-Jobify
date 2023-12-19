fa4aa075520023a980c17761d68d7293ad5175c5
Patrick He <96420427+hzz4343@users.noreply.github.com>
2023-12-18 21:10:40 -0600

    Set up jobs pages (edit and delete)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,12 +11,16 @@ import {
   AllJobs,
   Profile,
   Admin,
+  EditJob,
 } from './pages';
 import { action as registerAction } from './pages/Register';
 import { action as loginAction } from './pages/Login';
 import { loader as dashboardLoader } from './pages/DashboardLayout';
 import { action as addJobAction } from './pages/AddJob';
 import { loader as allJobsLoader } from './pages/AllJobs';
+import { loader as editJobLoader } from './pages/EditJob';
+import { action as editJobAction } from './pages/EditJob';
+import { action as deleteJobAction } from './pages/DeleteJob';
 
 export const checkDefaultTheme = () => {
   const isDarkTheme = localStorage.getItem('darkTheme') === 'true';
@@ -72,6 +76,16 @@ const router = createBrowserRouter([
           {
             path: 'admin',
             element: <Admin />,
+          },
+          {
+            path: 'edit-job/:id',
+            element: <EditJob />,
+            action: editJobAction,
+            loader: editJobLoader,
+          },
+          {
+            path: 'delete-job/:id',
+            action: deleteJobAction,
           },
         ],
       },
--- test.js
+++ test.js
@@ -1,28 +1,13 @@
-import Job from './Job';
-import Wrapper from '../assets/wrappers/JobsContainer';
+export const action = async ({ request, params }) => {
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
 
-import { useAllJobsContext } from '../pages/AllJobs';
-
-const JobsContainer = () => {
-  const { data } = useAllJobsContext();
-  const { jobs } = data;
-  if (jobs.length === 0) {
-    return (
-      <Wrapper>
-        <h2>No jobs to display...</h2>
-      </Wrapper>
-    );
+  try {
+    await customFetch.patch(`/jobs/${params.id}`, data);
+    toast.success('Job edited successfully');
+    return redirect('/dashboard/all-jobs');
+  } catch (error) {
+    toast.error(error.response.data.msg);
+    return error;
   }
-
-  return (
-    <Wrapper>
-      <div className="jobs">
-        {jobs.map((job) => {
-          return <Job key={job._id} {...job} />;
-        })}
-      </div>
-    </Wrapper>
-  );
 };
-
-export default JobsContainer;
